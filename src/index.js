3ef77a1640604e84637039c263ef6b47c11b9e94
Pavlina Mullerova <Pauline@Pavlinas-MacBook-Air.local>
2020-11-01 12:00:18 +0100

    mizeni navigace v hamburgeru

--- src/index.js
+++ src/index.js
@@ -2,3 +2,19 @@ import './index.html';
 import './style.css';
 
 console.log('funguju!');
+
+const navElm = document.querySelector('#nav-btn');
+
+navElm.addEventListener('click', () => {
+  const nav1 = document.querySelector('nav');
+  nav1.classList.toggle('nav-closed');
+});
+
+const navLinky = document.querySelectorAll('#nav-links');
+for (let i = 0; i < navLinky.length; i++) {
+  const navLink = navLinky[i];
+  navLink.addEventListener('click', () => {
+    const nav2 = document.querySelector('nav');
+    nav2.classList.add('nav-closed');
+  });
+}
